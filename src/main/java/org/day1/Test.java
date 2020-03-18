54ce3b36f78677f97f5c0dbf66b8bf6fd82ef548
0xMilk <1144502582@qq.com>
2020-03-18 16:17:13 +0800

    软引用例子

--- src/main/java/org/day1/Test.java
+++ src/main/java/org/day1/Test.java
@@ -1,15 +1,25 @@
 package org.day1;
 
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+
 public class Test {
     public static void main(String[] args) {
 
-        myInvoke(new MyCallBack() {
-            public void callBack(String key) {
-                System.out.println(key);
-            }
-        }, "哈哈哈");
+//        myInvoke(new MyCallBack() {
+//            public void callBack(String key) {
+//                System.out.println(key);
+//            }
+//        }, "哈哈哈");
+        ReferenceQueue<A> referenceQueue = new ReferenceQueue<A>();
+        byte[] bytes = new byte[1024 * 1024 * 6];
+        A aa = new A();
+        SoftReference<A> softReference = new SoftReference<A>(aa,referenceQueue);
+        byte[] bytes1 = new byte[1024 * 1024 * 4];
+
     }
-    public static void myInvoke(MyCallBack myCallBack, String world){
-        myCallBack.callBack(world);
-    }
+}
+
+class A{
+    public int a = 100;
 }
