b409afdeaad4db6ba462dc4731c46a2b848f452a
0xMilk <1144502582@qq.com>
2020-03-25 13:14:45 +0800

    测试

--- src/main/java/org/day10/Test.java
+++ src/main/java/org/day10/Test.java
@@ -2,9 +2,27 @@ package org.day10;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 4 , 2, 8 , 10, 21, 14, 23};
-        Demo d = new Demo();
-        int res = d.findKth(arr, 9);
+//        int[] arr = {1, 3, 4 , 2, 8 , 10, 21, 14, 23};
+//        Demo d = new Demo();
+//        int res = d.findKth(arr, 9);
+//        System.out.println(res);
+        int res = getDivision(10);
         System.out.println(res);
+    }
+
+    public static int getDivision(int n){
+        try{
+            n+=1;
+            if(n/0 > 0){
+                n+=10;
+            } else {
+                n-=10;
+            }
+            return n;
+        }catch(Exception e){
+            n++;
+        }
+        n++;
+        return n++;
     }
 }
