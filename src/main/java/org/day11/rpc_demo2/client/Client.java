7808c076112bb83012866e2142b743967f5388bb
Wq <1144502582@qq.com>
2020-11-02 15:39:51 +0800

    demo2-client代码优化

--- src/main/java/org/day11/rpc_demo2/client/Client.java
+++ src/main/java/org/day11/rpc_demo2/client/Client.java
@@ -1,6 +1,6 @@
 package org.day11.rpc_demo2.client;
 
-import org.day11.rpc_demo2.entity.UserInfo;
+import org.day11.rpc_demo2.client.entity.UserInfo;
 import org.day11.rpc_demo2.middle.MiddleService;
 
 /**
@@ -9,7 +9,8 @@ import org.day11.rpc_demo2.middle.MiddleService;
  */
 public class Client {
     public static void main(String[] args) throws Exception {
-        UserInfo u = MiddleService.getUerById(123);
-        System.out.println(u.getName());
+        //直接进行对应方法调用
+        UserInfo u = MiddleService.getUerById(111);
+        System.out.println("Rpc-Demo2--->客户端处理的结果: " + u.toString());
     }
 }
